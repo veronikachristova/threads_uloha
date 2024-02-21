7edf67131c1cd559add8dd7b088dc9001e6966d9
veronikachristova <smchristova@gmail.com>
2024-02-21 13:28:10 +0100

    commit 2

--- main.py
+++ main.py
@@ -7,17 +7,20 @@ def values_input(user_input):
 
 def max_value_function():
     max_value = max(values)
-    print(f"The maximum value is: {max_value}")
+    return max_value
 
 def min_value_function():
     min_value = min(values)
-    print(f"The minimum value is: {min_value}")
+    return min_value
 
 while True:
     user_input = int(input("Input a number: "))
     values_input(user_input)
     if len(values) == 5:
         break
+
+output1 = max_value_function()
+output2 = min_value_function()
 
 thread1 = threading.Thread(target=max_value_function)
 thread2 = threading.Thread(target=min_value_function)
@@ -28,5 +31,7 @@ thread2.start()
 thread1.join()
 thread2.join()
 
+print(f"The maximum value is: {output1}")
+print(f"The minimum value is: {output2}")
 print(values)
 
